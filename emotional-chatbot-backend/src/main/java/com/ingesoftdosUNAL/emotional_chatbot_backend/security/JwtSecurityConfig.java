4f2fbd086a1f24f9a2f75d4d442e8a7675fbcf76
Cristian Camilo <crtmilo@gmail.com>
2026-05-28 19:18:00 -0500

    fix: Correct cors usage to receive data from the frontend app

--- emotional-chatbot-backend/src/main/java/com/ingesoftdosUNAL/emotional_chatbot_backend/security/JwtSecurityConfig.java
+++ emotional-chatbot-backend/src/main/java/com/ingesoftdosUNAL/emotional_chatbot_backend/security/JwtSecurityConfig.java
@@ -6,9 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,9 +31,11 @@ public class JwtSecurityConfig {
     public SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/auth/**", "/conversations/**")
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated())
